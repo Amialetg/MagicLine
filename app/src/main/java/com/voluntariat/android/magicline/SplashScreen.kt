db91ddf50e3fde5f9fa06585c7947cfb6a42fc92
Unknown <rcpascualhector@gmail.com>
2018-06-26 22:53:04 +0200

    Before testing SplashScreen

--- app/src/main/java/com/voluntariat/android/magicline/SplashScreen.kt
+++ app/src/main/java/com/voluntariat/android/magicline/SplashScreen.kt
@@ -1,5 +1,44 @@
 package com.voluntariat.android.magicline
 
+import android.content.Intent
+import android.net.wifi.hotspot2.pps.HomeSp
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
 /**
  * Created by hector on 26/06/18.
  */
+class SplashScreen : AppCompatActivity(){
+    private var handler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000
+
+    internal val mRunnable: Runnable = Runnable {
+        if(!isFinishing){
+
+            val intent = Intent(applicationContext, PrincipalActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        handler = Handler()
+
+        handler!!.postDelayed(mRunnable,SPLASH_DELAY)
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+
+        if(handler != null){
+            handler!!.removeCallbacks(mRunnable)
+        }
+    }
+}

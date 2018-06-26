bb2e957b11f76a22d962423229ed06de5623880d
albert <riscoalbert@gmail.com>
2018-06-26 20:11:03 +0200

    Project and Principal Activity creation

---
+++ app/src/main/java/com/voluntariat/android/magicline/PrincipalActivity.kt
@@ -0,0 +1,12 @@
+package com.voluntariat.android.magicline
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}

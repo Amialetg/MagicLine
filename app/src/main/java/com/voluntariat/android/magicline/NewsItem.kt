fc90c0c23893e4293c90a385a27d3021a03fd278
albert <riscoalbert@gmail.com>
2018-07-16 13:11:16 +0200

    News section basic functionality

---
+++ app/src/main/java/com/voluntariat/android/magicline/NewsItem.kt
@@ -0,0 +1,3 @@
+package com.voluntariat.android.magicline
+
+class NewsItem(val title: String, val description: String)
--- app/src/main/java/com/voluntariat/android/magicline/PrincipalActivity.kt
+++ app/src/main/java/com/voluntariat/android/magicline/PrincipalActivity.kt
@@ -1,8 +1,12 @@
 package com.voluntariat.android.magicline
 
+import android.graphics.drawable.GradientDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.widget.TextView
 import com.voluntariat.android.magicline.utils.MyCounter
@@ -15,11 +19,17 @@ import java.util.concurrent.TimeUnit
 
 class PrincipalActivity : AppCompatActivity() {
 
+
+    //CountDown widgets
     lateinit var txtDies: TextView
     lateinit var txtHores: TextView
     lateinit var txtMin: TextView
     lateinit var txtSeg: TextView
     private val dateCursaString: String = "24.02.2019, 10:00"
+
+    //News section widgets
+    lateinit var newsRecyclerView: RecyclerView
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,15 +42,23 @@ class PrincipalActivity : AppCompatActivity() {
 
         initCountDown(counterTexts)
 
+        initNewsRecycler()
+
 
 
     }
 
     private fun initWidgets(): Array<TextView>{
+
+        //Countdown
         txtDies=findViewById(R.id.countdown_dies)
         txtHores=findViewById(R.id.countdown_hores)
         txtMin=findViewById(R.id.countdown_min)
         txtSeg=findViewById(R.id.countdown_seg)
+
+        //News
+        newsRecyclerView=findViewById(R.id.news_recycler)
+
 
         return arrayOf(txtDies, txtHores, txtMin, txtSeg)
     }
@@ -64,6 +82,25 @@ class PrincipalActivity : AppCompatActivity() {
 
 
         MyCounter(diff, 1000, txtDies).start()
+
+    }
+
+    private fun initNewsRecycler(){
+        val dataSet = arrayOf(NewsItem("Nou event en la programació", "In recent years people have realized the importance of proper diet and exercise, and recent surveys show that over the  otal ruta."), NewsItem("Segon event", "qewhvroeuyvfwouryvfowuer"))
+
+        val myNewsManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val myNewsAdapter = NewsAdapter(dataSet)
+
+        newsRecyclerView.layoutManager = myNewsManager
+        newsRecyclerView.adapter = myNewsAdapter
+
+        //Adding pager behaviour
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(newsRecyclerView)
+
+        //Adding the page indicators
+
+
 
     }
 }

c3b81a59373c741a68725edef94b786d9eaef618
Sharon Cabrera <sharon.cabrera@basetis.com>
2018-11-20 10:59:59 +0100

    comentario random

--- app/src/main/java/com/voluntariat/android/magicline/models/DetailModel.kt
+++ app/src/main/java/com/voluntariat/android/magicline/models/DetailModel.kt
@@ -1,5 +1,5 @@
 package com.voluntariat.android.magicline.models
 
 import android.media.Image
-
+//In a future we will need an url
 data class DetailModel(val img: Image, val title: String, val subtitle: String, val text: String, val link: String)

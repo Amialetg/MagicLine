fe020ee31f4d4f09ff6311d3bd3fb8c75a74f000
Andrea Serrano Calabuig <andrea.serrano@basetis.com>
2018-11-23 14:53:16 +0100

    screen has almost finished its design

--- app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/DetailFragment.kt
+++ app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/DetailFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.voluntariat.android.magicline.R
-
+import com.voluntariat.android.magicline.models.DetailModel
 
 
 class DetailFragment : Fragment() {
@@ -15,6 +15,15 @@ class DetailFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    private fun initDetailLayout(){
+        val dataSet = getDataset()
+    }
+
+
+    private fun getDataset(): DetailModel{
+        return DetailModel(title = "", link = "", subtitle = "", text = "")
     }
 }
 

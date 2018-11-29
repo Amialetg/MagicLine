c896fb96c25bbed85668194486847d723a2213ca
Sharon Cabrera <sharon.cabrera@basetis.com>
2018-11-29 13:07:02 +0100

    Settings screen layout  and IDs modified

--- app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/InfoFragment.kt
+++ app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/InfoFragment.kt
@@ -1,29 +1,19 @@
 package com.voluntariat.android.magicline.activities.main.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
 import com.voluntariat.android.magicline.R
+import kotlinx.android.synthetic.main.fragment_info.*
+import kotlinx.android.synthetic.main.layout_checkboxs_info.*
 
 class InfoFragment:Fragment(){
 
-
-    //Language widgets
-    lateinit var spanishRelativeLayout: RelativeLayout
-    lateinit var catalanRelativeLayout: RelativeLayout
-    lateinit var checkBoxCatalan: ImageView
-    lateinit var checkBoxSpanish: ImageView
-    lateinit var checkBoxCatalanText: TextView
-    lateinit var checkBoxSpanishText: TextView
     private var languagePressed=1 // 1--> SPANISH 2-->CATALAN
-    lateinit var inviteFriendsListener: TextView
-
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_info, container,  false)
@@ -32,59 +22,62 @@ class InfoFragment:Fragment(){
     override fun onStart() {
         super.onStart()
 
-        initWidgets()
-
         initLanguageSettings()
 
-        inviteFriendsListener()
-    }
-
-    private fun initWidgets(){
-        spanishRelativeLayout = view!!.findViewById(R.id.spanish_relative)
-        catalanRelativeLayout = view!!.findViewById(R.id.catalan_relative)
-        checkBoxCatalan = view!!.findViewById(R.id.checkbox_catalan)
-        checkBoxSpanish = view!!.findViewById(R.id.checkbox_spanish)
-        checkBoxCatalanText = view!!.findViewById(R.id.checkbox_catalan_text)
-        checkBoxSpanishText = view!!.findViewById(R.id.checkbox_spanish_text)
-        inviteFriendsListener = view!!.findViewById(R.id.textView_moreInfo_friends)
-
-
+        listener()
     }
 
     private fun initLanguageSettings(){
-        spanishRelativeLayout.setOnClickListener{
+        spanish_relative.setOnClickListener{
             if(languagePressed!=1){
-                checkBoxSpanish.setImageResource(R.drawable.checkbox_pressed)
-                checkBoxSpanishText.setTextAppearance(context, R.style.MoreInfoLanguagePressed)
+                checkbox_spanish.setImageResource(R.drawable.checkbox_pressed)
+                checkbox_spanish_text.setTextAppearance(context, R.style.MoreInfoLanguagePressed)
 
-                checkBoxCatalan.setImageResource(R.drawable.checkbox_not_pressed)
-                checkBoxCatalanText.setTextAppearance(context, R.style.MoreInfoLanguageNotPressed)
+                checkbox_catalan.setImageResource(R.drawable.checkbox_not_pressed)
+                checkbox_catalan_text.setTextAppearance(context, R.style.MoreInfoLanguageNotPressed)
 
                 languagePressed=1
             }
         }
 
-        catalanRelativeLayout.setOnClickListener{
+        catalan_relative.setOnClickListener{
             if(languagePressed!=2){
-                checkBoxCatalan.setImageResource(R.drawable.checkbox_pressed)
-                checkBoxCatalanText.setTextAppearance(context, R.style.MoreInfoLanguagePressed)
+                checkbox_catalan.setImageResource(R.drawable.checkbox_pressed)
+                checkbox_catalan_text.setTextAppearance(context, R.style.MoreInfoLanguagePressed)
 
-                checkBoxSpanish.setImageResource(R.drawable.checkbox_not_pressed)
-                checkBoxSpanishText.setTextAppearance(context, R.style.MoreInfoLanguageNotPressed)
+                checkbox_spanish.setImageResource(R.drawable.checkbox_not_pressed)
+                checkbox_spanish_text.setTextAppearance(context, R.style.MoreInfoLanguageNotPressed)
 
                 languagePressed=2
             }
         }
     }
 
-    private fun inviteFriendsListener(){
-        inviteFriendsListener.setOnClickListener{
+    private fun listener(){
+        val urlMagicLine= getString(R.string.magicLineWeb)
 
+        moreInfoFriendsTextView.setOnClickListener{
             val transaction = activity.supportFragmentManager.beginTransaction()
             transaction.replace(R.id.frame_layout, InviteFriendsFragment())
             transaction.commit()
-
         }
+        webMagicLineTextView.setOnClickListener{
+            callIntent(urlMagicLine)
+        }
+
+        basetisInfoTextView.setOnClickListener{
+            //Navegar al fragment de BaseTIS
+            print("Basetis Information")
+        }
+    }
+
+    private fun callIntent(url : String){
+
+        var intent : Intent
+        intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(intent)
+
     }
 
 

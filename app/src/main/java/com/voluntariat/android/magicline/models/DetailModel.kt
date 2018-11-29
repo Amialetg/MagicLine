3d11a337ec4e81072c9da1801d222a73a9040876
Andrea Serrano Calabuig <andrea.serrano@basetis.com>
2018-11-29 16:37:33 +0100

    comments and boolean added to change the style of the toptoolbar

--- app/src/main/java/com/voluntariat/android/magicline/models/DetailModel.kt
+++ app/src/main/java/com/voluntariat/android/magicline/models/DetailModel.kt
@@ -1,4 +1,7 @@
 package com.voluntariat.android.magicline.models
 
-//In a future we will need an url
-data class DetailModel(val title: String, val subtitle: String, val text: String, val link: String)
+import android.graphics.drawable.Drawable
+import java.io.Serializable
+
+//By default the colors of the toolbar are black, if you want to set it to white you have to use the "isBlack" boolean when you call the DetailModel()
+data class DetailModel(val title: String, val subtitle: String, val textBody: String, val link: String, val toolbarImg: Drawable, val isBlack: Boolean = true) : Serializable

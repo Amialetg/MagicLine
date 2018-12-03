1b7834cd908eab1a8a04acfd7cf91087ad6c7337
Andrea Serrano Calabuig <andrea.serrano@basetis.com>
2018-12-03 11:00:17 +0100

    refactor ml fragment without findviewbyid

--- app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/MagicLineFragment.kt
+++ app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/MagicLineFragment.kt
@@ -12,7 +12,6 @@ import android.support.v7.widget.PagerSnapHelper
 import android.support.v7.widget.RecyclerView
 import java.util.*
 import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,6 +26,8 @@ import com.voluntariat.android.magicline.models.DetailModel
 import com.voluntariat.android.magicline.models.NewsModel
 import com.voluntariat.android.magicline.models.ProgrammingModel
 import kotlinx.android.synthetic.main.layout_a_fons.*
+import kotlinx.android.synthetic.main.layout_countdown.*
+import kotlinx.android.synthetic.main.layout_news.*
 import kotlinx.android.synthetic.main.layout_recaudats_participants.*
 import kotlinx.android.synthetic.main.layout_rrss.*
 import java.text.SimpleDateFormat
@@ -34,35 +35,14 @@ import java.text.SimpleDateFormat
 
 class MagicLineFragment : Fragment() {
 
-    //CountDown widgets
-    lateinit var txtDies: TextView
-    lateinit var txtHores: TextView
-    lateinit var txtMin: TextView
-    lateinit var txtSeg: TextView
     private lateinit var dateCursaString: String
 
     //Programming section widgets
     lateinit var progRecyclerView: RecyclerView
 
-    //News section widgets
-    lateinit var newsRecyclerView: RecyclerView
-    lateinit var leftArrowView: RelativeLayout
-    lateinit var rightArrowView: RelativeLayout
-    lateinit var recyclerViewIndicator: com.kingfisher.easyviewindicator.RecyclerViewIndicator
-
-    /*
-     * Setting the corresponding view
-     */
+    //Setting the corresponding view
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_magic_line, container,  false)
-    }
-
-    /*
-     * Once the view is ready we can initialize all components
-     */
-
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-
+        return inflater?.inflate(R.layout.fragment_magic_line, container, false)
     }
 
     override fun onStart() {
@@ -92,28 +72,15 @@ class MagicLineFragment : Fragment() {
         }
     }
 
-    private fun initWidgets():Array<TextView>{
-        //Countdown
-        txtDies=view!!.findViewById(R.id.countdown_dies)
-        txtHores=view!!.findViewById(R.id.countdown_hores)
-        txtMin=view!!.findViewById(R.id.countdown_min)
-        txtSeg=view!!.findViewById(R.id.countdown_seg)
+    private fun initWidgets(): Array<TextView> {
 
         //cursa date
-        dateCursaString= getString(R.string.cursa_date)
+        dateCursaString = getString(R.string.cursa_date)
 
-        //News
-        newsRecyclerView=view!!.findViewById(R.id.news_recycler)
-        leftArrowView=view!!.findViewById(R.id.left_arrow_relative)
-        rightArrowView=view!!.findViewById(R.id.right_arrow_relative)
-        recyclerViewIndicator=view!!.findViewById(R.id.news_pager_indicator)
-
-        return arrayOf(txtDies, txtHores, txtMin, txtSeg)
-
-
+        return arrayOf(countdown_dies, countdown_hores, countdown_min, countdown_seg)
     }
 
-    private fun initCountDown(txtDies: Array<TextView>){
+    private fun initCountDown(txtDies: Array<TextView>) {
 
         //Utilitzem el formatter per aconseguir l'objecte Date
         var formatter = SimpleDateFormat("dd.MM.yyyy, HH:mm")
@@ -123,17 +90,17 @@ class MagicLineFragment : Fragment() {
         var dateCursa: Date = formatter.parse(dateCursaString)
 
         //pasem a long les dates
-        var currentLong:Long=currentTime.time
-        var cursaLong:Long=dateCursa.time
+        var currentLong: Long = currentTime.time
+        var cursaLong: Long = dateCursa.time
 
         //trobem el temps restant en long
-        var diff:Long=cursaLong-currentLong
+        var diff: Long = cursaLong - currentLong
 
         MyCounter(diff, 1000, txtDies).start()
 
     }
 
-    private fun initProgrammingCards(){
+    private fun initProgrammingCards() {
 
         //data Test
         val events = ArrayList<ProgrammingModel>()
@@ -155,68 +122,69 @@ class MagicLineFragment : Fragment() {
         val myNewsManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
         val myNewsAdapter = NewsAdapter(dataSet)
 
-        newsRecyclerView.layoutManager = myNewsManager
-        newsRecyclerView.adapter = myNewsAdapter
+        news_recycler.layoutManager = myNewsManager
+        news_recycler.adapter = myNewsAdapter
 
         //Adding pager behaviour
         val snapHelper = PagerSnapHelper()
-        newsRecyclerView.onFlingListener = null //<-- We add this line to avoid the app crashing when returning from the background
-        snapHelper.attachToRecyclerView(newsRecyclerView)
+        news_recycler.onFlingListener = null //<-- We add this line to avoid the app crashing when returning from the background
+        snapHelper.attachToRecyclerView(news_recycler)
 
         //Adding the page indicators
-        recyclerViewIndicator.setRecyclerView(newsRecyclerView)
+        news_pager_indicator.setRecyclerView(news_recycler)
 
         //Adding buttons listeners
         initArrowsListeners(myNewsManager)
     }
 
-    private fun initArrowsListeners(mLayoutManager: LinearLayoutManager){
-        
-        rightArrowView.setOnClickListener{
-            val totalItemCount= newsRecyclerView.adapter.itemCount
+    private fun initArrowsListeners(mLayoutManager: LinearLayoutManager) {
 
-            if(totalItemCount<0) return@setOnClickListener
+        right_arrow_relative.setOnClickListener {
+            val totalItemCount = news_recycler.adapter.itemCount
+
+            if (totalItemCount < 0) return@setOnClickListener
 
             val lastVisibleItemIndex = mLayoutManager.findLastVisibleItemPosition()
 
-            if(lastVisibleItemIndex>=totalItemCount) return@setOnClickListener
+            if (lastVisibleItemIndex >= totalItemCount) return@setOnClickListener
 
-            mLayoutManager.smoothScrollToPosition(newsRecyclerView, null, lastVisibleItemIndex+1)
+            mLayoutManager.smoothScrollToPosition(news_recycler, null, lastVisibleItemIndex + 1)
         }
-        leftArrowView.setOnClickListener {
-            val totalItemCount= newsRecyclerView.adapter.itemCount
 
-            if(totalItemCount<0) return@setOnClickListener
+        left_arrow_relative.setOnClickListener {
+            val totalItemCount = news_recycler.adapter.itemCount
+
+            if (totalItemCount < 0) return@setOnClickListener
 
             val lastVisibleItemIndex = mLayoutManager.findLastVisibleItemPosition()
 
-            if(lastVisibleItemIndex<=0) return@setOnClickListener
+            if (lastVisibleItemIndex <= 0) return@setOnClickListener
 
-            mLayoutManager.smoothScrollToPosition(newsRecyclerView, null, lastVisibleItemIndex-1)
+            mLayoutManager.smoothScrollToPosition(news_recycler, null, lastVisibleItemIndex - 1)
         }
     }
 
-    private fun initAfonsListeners(){
+    private fun initAfonsListeners() {
 
         val dataModelEssential = DetailModel(title = getString(R.string.essentials_title), subtitle = getString(R.string.essentials_subtitle), textBody = getString(R.string.essentials_body), link = getString(R.string.essentials_viewOnWeb), toolbarImg = getDrawable(context, about_us), isBlack = false)
         val dataModelDestiny = DetailModel(title = getString(R.string.donations_title), subtitle = getString(R.string.donations_subtitle), textBody = getString(R.string.donations_body), link = getString(R.string.donations_viewOnWeb), toolbarImg = getDrawable(context, about_us))
         val dataModelSantJoan = DetailModel(title = getString(R.string.sjd_title), subtitle = getString(R.string.sjd_subtitle), textBody = getString(R.string.sjd_body), link = getString(R.string.sjd_viewOnWeb), toolbarImg = getDrawable(context, about_us))
 
-        info_essentials_button.setOnClickListener{
+        info_essentials_button.setOnClickListener {
             val transaction = activity.supportFragmentManager.beginTransaction()
             transaction.replace(R.id.frame_layout, DetailFragment.newInstance(dataModelEssential))
             transaction.addToBackStack("infoEssentialsButton")
             transaction.commit()
         }
 
-        info_donations_destiny_button.setOnClickListener{
+        info_donations_destiny_button.setOnClickListener {
             val transaction = activity.supportFragmentManager.beginTransaction()
             transaction.replace(R.id.frame_layout, DetailFragment.newInstance(dataModelDestiny))
             transaction.addToBackStack("infoDestinyButton")
             transaction.commit()
         }
 
-        info_sjd_button.setOnClickListener{
+        info_sjd_button.setOnClickListener {
             val transaction = activity.supportFragmentManager.beginTransaction()
             transaction.replace(R.id.frame_layout, DetailFragment.newInstance(dataModelSantJoan))
             transaction.addToBackStack("infoSjdButton")
@@ -224,56 +192,51 @@ class MagicLineFragment : Fragment() {
         }
     }
 
-    private fun initRRSSListeners(){
+    private fun initRRSSListeners() {
 
         val urlFacebook = getString(R.string.url_facebook)
         val urlGoogle = getString(R.string.url_google)
         val urlTwitter = getString(R.string.url_twitter)
 
 
-        fb_button.setOnClickListener{
+        fb_button.setOnClickListener {
             callIntent(urlFacebook)
         }
 
-        insta_button.setOnClickListener{
+        insta_button.setOnClickListener {
             callIntent(urlGoogle)
         }
 
-        twitter_button.setOnClickListener{
+        twitter_button.setOnClickListener {
             callIntent(urlTwitter)
         }
 
 
     }
 
-    private fun callIntent(url : String){
-
-        var intent : Intent
-
+    private fun callIntent(url: String) {
+        var intent: Intent
         intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         context.startActivity(intent)
-
     }
 
-    class MarginItemDecoration(private val spaceHeight: Int) : RecyclerView.ItemDecoration() {
-        override fun getItemOffsets(outRect: Rect, view: View,
-                                    parent: RecyclerView, state: RecyclerView.State) {
-            with(outRect) {
-
-                if(parent.getChildAdapterPosition(view) == parent.adapter.itemCount-1){
-                    right = spaceHeight*2
-                    left = spaceHeight
-                }
-                else if(parent.getChildAdapterPosition(view) == 0){
-                    left = spaceHeight*2
-                    right = spaceHeight
-                }
-                else{
-                    left =  spaceHeight
-                    right = spaceHeight
-                }
-            }
-        }
-    }
+//    class MarginItemDecoration(private val spaceHeight: Int) : RecyclerView.ItemDecoration() {
+//        override fun getItemOffsets(outRect: Rect, view: View,
+//                                    parent: RecyclerView, state: RecyclerView.State) {
+//            with(outRect) {
+//
+//                if (parent.getChildAdapterPosition(view) == parent.adapter.itemCount - 1) {
+//                    right = spaceHeight * 2
+//                    left = spaceHeight
+//                } else if (parent.getChildAdapterPosition(view) == 0) {
+//                    left = spaceHeight * 2
+//                    right = spaceHeight
+//                } else {
+//                    left = spaceHeight
+//                    right = spaceHeight
+//                }
+//            }
+//        }
+//    }
 }

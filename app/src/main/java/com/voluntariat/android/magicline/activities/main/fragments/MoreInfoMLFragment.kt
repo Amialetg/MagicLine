131e9db72d7d1653fa087b5a60ab24e048a5b551
Andrea Serrano Calabuig <andrea.serrano@basetis.com>
2018-12-04 16:16:10 +0100

    doing some bold Text combined with normal text using html from html

--- app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/MoreInfoMLFragment.kt
+++ app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/MoreInfoMLFragment.kt
@@ -1,19 +1,28 @@
 package com.voluntariat.android.magicline.activities.main.fragments
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.text.Html
+import android.text.SpannableString
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.voluntariat.android.magicline.R
 import com.voluntariat.android.magicline.R.drawable.*
-import com.voluntariat.android.magicline.activities.main.MainActivity
 import com.voluntariat.android.magicline.models.MoreInfoMLModel
 import kotlinx.android.synthetic.main.toolbar_appbar_top.*
 import kotlinx.android.synthetic.main.toolbar_appbar_top.view.*
-import kotlinx.android.synthetic.main.toolbar_bottom_nav.*
+import com.voluntariat.android.magicline.R.string.*
+import kotlinx.android.synthetic.main.fragment_more_info_ml.view.*
+import android.text.Spannable
+import android.text.style.StyleSpan
+
+
+
 
 class MoreInfoMLFragment : Fragment() {
 
@@ -22,7 +31,7 @@ class MoreInfoMLFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        moreInfoMLView =  inflater.inflate(R.layout.fragment_more_info_ml, container, false)
+        moreInfoMLView = inflater.inflate(R.layout.fragment_more_info_ml, container, false)
         initToolbar()
         return moreInfoMLView
     }
@@ -32,6 +41,12 @@ class MoreInfoMLFragment : Fragment() {
         moreInfoMLView.topToolbar.title = getString(R.string.ml)
         moreInfoMLView.topToolbar.navigationIcon = ContextCompat.getDrawable(context, ic_black_cross)
         moreInfoMLView.topToolbar.setNavigationOnClickListener { activity.onBackPressed() }
-    }
 
+        val text: String = getString(walk_text_1) + " "
+        val text2: String =  "<b>"+ getString(walk_text_2)+ "</b>" + " "
+        val text3: String = getString(walk_text_3) + " "
+        val text4: String = "<b>"+ getString(walk_text_4) +"</b>"
+        val textView = moreInfoMLView.firstWalkText
+        textView.text = Html.fromHtml(text + text2 + text3 + text4)
+    }
 }

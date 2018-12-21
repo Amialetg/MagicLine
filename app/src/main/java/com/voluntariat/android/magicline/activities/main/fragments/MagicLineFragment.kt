8c35b222583c549577989c9b1ff037c0cfc41dc2
Andrea Serrano Calabuig <andrea.serrano@basetis.com>
2018-12-21 11:21:40 +0100

    changed rss links

--- app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/MagicLineFragment.kt
+++ app/src/main/java/com/voluntariat/android/magicline/activities/main/fragments/MagicLineFragment.kt
@@ -239,7 +239,7 @@ class MagicLineFragment : BaseFragment() {
     private fun initRRSSListeners() {
 
         val urlFacebook = getString(R.string.url_facebook)
-        val urlGoogle = getString(R.string.url_google)
+        val urlInstagram = getString(R.string.url_instagram)
         val urlTwitter = getString(R.string.url_twitter)
 
 
@@ -248,7 +248,7 @@ class MagicLineFragment : BaseFragment() {
         }
 
         insta_button.setOnClickListener {
-            callIntent(urlGoogle)
+            callIntent(urlInstagram)
         }
 
         twitter_button.setOnClickListener {

79913263243f3b5a433ac93210eb37dc0ada3028
Yvette Hernandez <yvhalo@gmail.com>
2019-01-02 15:02:40 +0100

    Uses language utils for setting default language on app install

--- app/src/main/java/com/voluntariat/android/magicline/activities/main/general/MyApplication.kt
+++ app/src/main/java/com/voluntariat/android/magicline/activities/main/general/MyApplication.kt
@@ -3,6 +3,10 @@ package com.voluntariat.android.magicline.activities.main.general
 import android.app.Application
 import com.onesignal.OneSignal
 import com.voluntariat.android.magicline.utils.ExampleNotificationOpenedHandler
+import com.voluntariat.android.magicline.utils.getLocaleTagForString
+import com.voluntariat.android.magicline.utils.updateBaseContextLocale
+import com.voluntariat.android.magicline.utils.updatePreferencesLanguage
+import java.util.*
 
 class MyApplication : Application() {
 
@@ -13,5 +17,9 @@ class MyApplication : Application() {
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .setNotificationOpenedHandler(ExampleNotificationOpenedHandler(this))
                 .init()
+
+        val language = getLocaleTagForString(Locale.getDefault().language)
+        updatePreferencesLanguage(this, language)
+        updateBaseContextLocale(language)
     }
 }

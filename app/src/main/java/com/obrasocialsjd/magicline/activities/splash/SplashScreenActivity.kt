00c6517ecb52242639340184a75f35485247fb74
Yvette Hernandez <yvhalo@gmail.com>
2019-01-02 16:02:26 +0100

    Fixing cleartext api call error and moves authentication to splash screen
    (cherry picked from commit a36102d)

--- app/src/main/java/com/obrasocialsjd/magicline/activities/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/obrasocialsjd/magicline/activities/splash/SplashScreenActivity.kt
@@ -4,24 +4,34 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import com.obrasocialsjd.magicline.R
 import com.obrasocialsjd.magicline.activities.main.general.MainActivity
+import com.obrasocialsjd.magicline.data.MagicLineRepositoryImpl
+import com.obrasocialsjd.magicline.data.Result
+import com.obrasocialsjd.magicline.db.MagicLineDB
 
 /**
  * Created by hector on 26/06/18.
  */
-class SplashScreenActivity : Activity(){
+class SplashScreenActivity : Activity() {
     private var handler: Handler? = null
-    private val SPLASH_DELAY: Long = 200
+    private val splashDelay: Long = 200
 
-    internal val mRunnable: Runnable = Runnable {
+    private val mRunnable: Runnable = Runnable {
         if(!isFinishing){
-
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-
+            authenticate()
         }
+    }
+
+    private fun authenticate() {
+        val repo = MagicLineRepositoryImpl(MagicLineDB.getDatabase(applicationContext))
+        repo.authenticate(
+                "apiml",
+                "4p1ml2018"
+        ) { result -> when (result) {
+            is Result.Success -> {
+                authenticationSuccess()
+            }
+        } }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,15 +41,20 @@ class SplashScreenActivity : Activity(){
 
         handler = Handler()
 
-        handler!!.postDelayed(mRunnable,SPLASH_DELAY)
+        handler!!.postDelayed(mRunnable, splashDelay)
     }
 
     public override fun onDestroy() {
-
         super.onDestroy()
 
         if(handler != null){
             handler!!.removeCallbacks(mRunnable)
         }
+    }
+
+    private fun authenticationSuccess() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }

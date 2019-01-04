a1071b0cba61e31f0c6d0247c0966c9130443f08
Yvette Hernandez <yvhalo@gmail.com>
2019-01-04 10:51:17 +0100

    upgrades room database version

--- app/src/main/java/com/obrasocialsjd/magicline/db/MagicLineDB.kt
+++ app/src/main/java/com/obrasocialsjd/magicline/db/MagicLineDB.kt
@@ -16,7 +16,7 @@ import com.obrasocialsjd.magicline.db.dao.DonationsDAO
 import com.obrasocialsjd.magicline.db.dao.PostDao
 import com.obrasocialsjd.magicline.db.dao.TeamsMarkersDAO
 
-@Database(entities = [PostsItem::class, DonationsDBModel::class, TeamsDBModel::class], version = 1, exportSchema = false)
+@Database(entities = [PostsItem::class, DonationsDBModel::class, TeamsDBModel::class], version = 2, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class MagicLineDB : RoomDatabase() {
     abstract fun postDao(): PostDao

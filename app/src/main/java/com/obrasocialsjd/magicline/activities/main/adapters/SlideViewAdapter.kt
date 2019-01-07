821b989a3a79dca5d01ddd6054052841a7b4fdce
Sharon Cabrera <sharon.cabrera@basetis.com>
2019-01-07 16:16:18 +0100

    Unnecessary line break removed, xml modified

--- app/src/main/java/com/obrasocialsjd/magicline/activities/main/adapters/SlideViewAdapter.kt
+++ app/src/main/java/com/obrasocialsjd/magicline/activities/main/adapters/SlideViewAdapter.kt
@@ -14,24 +14,15 @@ class SlideViewAdapter(private var dataSet : List<Int> = listOf()) : RecyclerVie
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var image: ImageView = itemView.imgDetail
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_details, parent, false)
 
         return ViewHolder(itemView)
     }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        Picasso
-                .get()
-                .load(dataSet[position])
-                .resize(0,350)
-                .centerInside()
-                .into(holder.image)
+        Picasso.get().load(dataSet[position]).resize(0,350).centerInside().into(holder.image)
     }
     override fun getItemCount() = dataSet.size
-
     fun loadItems (images: List<Int>) {
         dataSet = images
     }

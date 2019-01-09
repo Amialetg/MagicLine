ef09e101533b1bcc9aa1e15f8641b198d9eb81f9
Yvette Hernandez <yvhalo@gmail.com>
2019-01-09 15:38:18 +0100

    Merge branches 'Develop' and 'Diff/removesBottomBar' of https://github.com/riscuus/MagicLine into Diff/removesBottomBar
    # Conflicts:
    #	app/src/main/java/com/obrasocialsjd/magicline/utils/Constants.kt

--- app/src/main/java/com/obrasocialsjd/magicline/activities/main/adapters/SlideViewAdapter.kt
+++ app/src/main/java/com/obrasocialsjd/magicline/activities/main/adapters/SlideViewAdapter.kt
@@ -6,6 +6,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.obrasocialsjd.magicline.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_details.view.*
 
 class SlideViewAdapter(private var dataSet : List<Int> = listOf()) : RecyclerView.Adapter<SlideViewAdapter.ViewHolder>() {
 
